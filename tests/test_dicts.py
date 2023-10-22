7a80f4565552a0880f14184189ce7ee0a064ecdd
Alina Mordovina <alina182@yandex.ru>
2023-10-22 05:16:03 +0400

    Add conftest.py and upgrade test_dicts.py

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,10 +1,9 @@
 from utils import dicts
 
 
-def test_get_val():
-    data = {"vcs": "mercurial"}
-    assert dicts.get_val(data, "vcs") == "mercurial"
-    assert dicts.get_val(data, "vcs", "git") == "mercurial"
+def test_get_val(dict_fixture):
+    assert dicts.get_val(dict_fixture, "vcs") == "mercurial"
+    assert dicts.get_val(dict_fixture, "vcs", "git") == "mercurial"
     assert dicts.get_val({}, "vcs", "git") == "git"
     assert dicts.get_val({}, "vcs", "bazaar") == "bazaar"
-    assert dicts.get_val(data, "param") == "git"
+    assert dicts.get_val(dict_fixture, "param") == "git"
